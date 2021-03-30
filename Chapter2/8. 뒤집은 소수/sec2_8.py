bf49a45273b85f9f26f3ffe56a7a80ad49e809a5
sunhong-lee <64203433+sunhong-lee@users.noreply.github.com>
2021-03-30 22:06:26 +0900

    이선홍

---
+++ Chapter2/8. 뒤집은 소수/sec2_8.py
@@ -0,0 +1,34 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def isPrime(x):
+	if x==1:
+		print("1is not prime num")
+	re=True
+	for i in range(2,x):
+		if x%i==0:
+			re=False
+			break
+		else:
+			re=True
+	return re
+
+>>> def rev(x):
+	x=str(x)
+	xx=0
+	for i in range(len(x)):
+		xx+=x[len(x)-1-i]
+	xx=int(xx)
+	return xx
+
+>>> rev_dat=list()
+>>> for i in range(n):
+	rev_dat.append(rev(i))
+
+	
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    for i in range(n):
+NameError: name 'n' is not defined
+>>> for i,x in enumerate(rev_dat):
+	if isPrime(x)==True:
+		print(x)
