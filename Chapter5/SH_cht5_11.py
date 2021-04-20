c20154312dac87182d9788790412bbcaf61896a4
Sang Hyuk Lee <49121293+SaeSimcheon@users.noreply.github.com>
2021-04-20 18:53:22 +0900

    Add files via upload

---
+++ Chapter5/SH_cht5_11.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin=open("input.txt", "r")
+
+#seq=[int(input()) for i in range(10)]
+
+
+
+hip = list()
+
+while True:
+    i = int(input())
+    #print(i)
+    if i not in [-1,0]:
+        hip.append(i)
+    elif i == -1 :
+        break
+    else :
+        if len(hip) ==0:
+            print(-1)
+        else:
+            print(max(hip))
+            del hip[hip.index(max(hip))]
