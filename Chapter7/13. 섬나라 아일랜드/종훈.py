731f4a160437c598ee0566c074ef0ebda709a44c
yjhoon2 <whdgnsl06@naver.com>
2021-05-15 14:56:33 +0900

    5/15 update

---
+++ Chapter7/13. 섬나라 아일랜드/종훈.py
@@ -0,0 +1,62 @@
+import sys
+#sys.stdin = open("input.txt", "rt")
+input = sys.stdin.readline  # 입력량이 많을 때 입력 속도가 빨라짐
+#s = input().rstrip(): # readline 쓰면서 문자열을 읽을 때 문자열 끝에 줄바꿈 기호까지 읽기 때문에 탈피해야함 
+
+
+
+
+#### 정답이 걍 내꺼랑 똑같네
+# 혹시 내꺼 배껴서 만듦???
+from collections import deque
+
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, 1, 1, 1, 0, -1, -1, -1]
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+cnt = 0
+Q = deque()
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            board[i][j] = 0
+            Q.append((i, j))
+            while Q:
+                tmp = Q.popleft()
+                for k in range(8):
+                    x = tmp[0] + dx[k]
+                    y = tmp[1] + dy[k]
+                    if 0 <= x < n and 0 <= y < n and board[x][y] == 1:
+                        board[x][y] = 0
+                        Q.append((x, y))
+            cnt += 1
+print(cnt)
+
+'''
+## 쉽노~~~~~~
+dx = [-1, 0, 1, 0, -1, -1, 1, 1]
+dy = [0, 1, 0, -1, 1, -1, 1, -1]
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+cnt = 0
+Q = deque()
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            cnt += 1
+            Q.append((i, j))
+            while Q:
+                tmp = Q.popleft()
+                for k in range(8):
+                    xx = tmp[0] + dx[k]
+                    yy = tmp[1] + dy[k]
+                    if 0 <= xx < n and 0 <= yy < n and board[xx][yy] == 1:
+                        board[xx][yy] = 0
+                        Q.append((xx, yy))
+print(cnt)
+'''
+
