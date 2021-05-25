200816e2b298e1f8ae3efccc9df172db03f4a4b6
sunhong-lee <64203433+sunhong-lee@users.noreply.github.com>
2021-05-25 20:22:48 +0900

    Update sunhong.py

--- Chapter8/7, 8. 알리바바와 40인의 도둑/sunhong.py
+++ Chapter8/7, 8. 알리바바와 40인의 도둑/sunhong.py
@@ -25,7 +25,6 @@ for i in range(1,n):
         if 0<=j-1<n or 0<=i-1<n:
             dy[i][j]=min(dy[i][j-1],dy[i-1][j])+base[i][j]
 
-print(base[n-1][n-1])
+print(dy[n-1][n-1])
 
 
-## 틀림 어디가 틀렸는지 모르겠음
